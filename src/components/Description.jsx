7f6ea51d0405f14ab177a9d6dac2b66f08438af3
asthamishra02 <astham2003@gmail.com>
2023-11-02 21:00:52 +0530

    css added

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import React from 'react';
+import "./Description.css";
+import {FaArrowDown} from "react-icons/fa";
 const Description = () => {
   return (
-    <div>
-      
-    </div>
-  )
+        <div className='section section__descriptions'>
+            <div className='card'>
+              <div className='description__card-icon'>
+                <FaArrowDown/>
+                <small>min</small>
+                </div>
+                <h2> 32 °C </h2>
+            </div>
+
+        </div>
+  );
 }
 
-export default Description
+export default Description;
